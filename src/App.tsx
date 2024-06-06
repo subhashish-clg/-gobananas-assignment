f696c919993482feb2a03a7583c91a617fac1775
Subhashish Nabajja <subhashishnabajja1575643@gmail.com>
2024-06-06 21:55:13 +0530

    Add table logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,79 @@
 import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
-import DataTable from "./components/DataTable";
+import DataTable from "./components/DataTable/DataTable";
+import { Data, HeadCell } from "./components/DataTable/types";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function createData(
+  id: number,
+  name: string,
+  calories: number,
+  fat: number,
+  carbs: number,
+  protein: number
+): Data {
+  return {
+    id,
+    name,
+    calories,
+    fat,
+    carbs,
+    protein,
+  };
+}
+
+const headCells: readonly HeadCell[] = [
+  {
+    id: "id",
+    numeric: true,
+    disablePadding: false,
+    label: "ID",
+  },
+  {
+    id: "userId",
+    numeric: true,
+    disablePadding: false,
+    label: "User ID",
+  },
+  {
+    id: "title",
+    numeric: true,
+    disablePadding: false,
+    label: "Title",
+  },
+  {
+    id: "body",
+    numeric: true,
+    disablePadding: false,
+    label: "Body",
+  },
+];
+
+interface Post extends Data {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
 
 function App() {
+  const [data, setData] = useState<Post[]>([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await axios.get(
+          "https://jsonplaceholder.typicode.com/posts"
+        );
+
+        setData(data.data);
+      } catch {
+        setData([]);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <Box>
       <Box>
@@ -14,7 +86,11 @@ function App() {
         </AppBar>
       </Box>
 
-      <DataTable />
+      <DataTable
+        searchableFields={["title", "body"]}
+        headCells={headCells}
+        rows={data}
+      />
     </Box>
   );
 }
---
+++ src/components/DataTable/TableToolbar.tsx
@@ -0,0 +1,75 @@
+import Toolbar from "@mui/material/Toolbar";
+import { alpha } from "@mui/material/styles";
+import { EnhancedTableToolbarProps } from "./types";
+import Typography from "@mui/material/Typography";
+import Tooltip from "@mui/material/Tooltip";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Box from "@mui/material/Box";
+import InputAdornment from "@mui/material/InputAdornment";
+import TextField from "@mui/material/TextField";
+import { Search } from "@mui/icons-material";
+
+export default function EnhancedTableToolbar(props: EnhancedTableToolbarProps) {
+  const { numSelected, value, onChange } = props;
+
+  return (
+    <Toolbar
+      sx={{
+        pl: { sm: 2 },
+        pr: { xs: 1, sm: 1 },
+        ...(numSelected > 0 && {
+          bgcolor: (theme) =>
+            alpha(
+              theme.palette.primary.main,
+              theme.palette.action.activatedOpacity
+            ),
+        }),
+      }}
+    >
+      {numSelected > 0 ? (
+        <Typography
+          sx={{ flex: "1 1 100%" }}
+          color="inherit"
+          variant="subtitle1"
+          component="div"
+        >
+          {numSelected} selected
+        </Typography>
+      ) : (
+        <Typography
+          sx={{ flex: "1 1 100%" }}
+          variant="h6"
+          id="tableTitle"
+          component="div"
+        >
+          Nutrition
+        </Typography>
+      )}
+      {numSelected > 0 ? (
+        <Tooltip title="Delete">
+          <IconButton>
+            <DeleteIcon />
+          </IconButton>
+        </Tooltip>
+      ) : (
+        <Box>
+          <TextField
+            value={value}
+            onChange={onChange}
+            id="outlined-basic"
+            placeholder="Search"
+            variant="outlined"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <Search />
+                </InputAdornment>
+              ),
+            }}
+          />
+        </Box>
+      )}
+    </Toolbar>
+  );
+}
